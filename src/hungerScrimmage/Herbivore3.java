6413b4e9bd335d9a86de79893ef30de64adeb606
ehennenfent <ehennenfent@imsa.edu>
2015-05-22 23:30:49 -0500

    Herbivore 3
    Implemented Herbivore 3 w/ empty space targeting and predator avoidance.

--- src/hungerScrimmage/Herbivore3.java
+++ src/hungerScrimmage/Herbivore3.java
@@ -1,6 +1,5 @@
 package hungerScrimmage;
 
-
 import java.awt.Color;
 import java.util.List;
 
@@ -9,6 +8,15 @@ import theHungerGames.*;
 public class Herbivore3 extends Herbivore {
 
 	static private Color color = new Color(255, 240, 97);
+	static private final double FOOD_SCALAR = 10;
+	static private final double FOOD_LB_SCALAR = 10;
+	static private final double HIDE_SCALAR = 10;
+	static private final double TARGET_PROBABILITY = .1;
+	static private final double DENSITY_DISTANCE = 6;
+	static private final double MAX_DENSITY = 75;
+	static private final int MONTE_CARLO_POINTS = 15;
+	static private Cell targetCell;
+	static private Cell initialCell;
 	
 	@Override
 	protected Color getColor() {
@@ -17,7 +25,7 @@ public class Herbivore3 extends Herbivore {
 
 	@Override
 	public String getName() {
-		return "Herbivore 3";
+		return "Shitting Panda";
 	}
 
 	@Override
@@ -28,40 +36,68 @@ public class Herbivore3 extends Herbivore {
 				return new Mate(ani);
 			}
 		}
-
-		if (getCell().howMuchFood() > 5) {
-			return new HerbivoreEat();
-		} else {
-			return new Move(Direction.randomDirection()); 
-		}
+			if (getCell().howMuchFood() > (FOOD_SCALAR * 0.26999308176309406) || getEnergyReserve() < (FOOD_LB_SCALAR * 0.42994026228221666)) {
+				return new HerbivoreEat();
+			} 
+		return doMovingBehavior();
 	}
-
+	
+	private Move doMovingBehavior(){
+		if(getArena().getNDays() % 360 == 0){
+			double min = Integer.MAX_VALUE;
+			for(int i = 0; i < MONTE_CARLO_POINTS; i++){
+				Cell target = new Cell(getArena(),
+						Arena.getRandom().nextInt(getArena().getXDim()),
+						Arena.getRandom().nextInt(getArena().getYDim()));
+				if(getDensity(target) <= min){
+					targetCell = target;
+					initialCell = getCell();
+				}
+			}
+		}
+			for(Animal ani : getArena().getAllAnimals()){
+				if(checkMateability(ani)){
+					if(Arena.getRandom().nextDouble() < 0.45665207580042166){
+						return new MoveToward(getCell(), ani.getCell(), true);
+					}
+				}
+				if(ani instanceof Carnivore && distance(ani.getCell(), getCell()) < (HIDE_SCALAR * 0.4361667272802573)){
+					return new MoveToward(getCell(), ani.getCell(), false);
+				}
+			}
+//			Cell initialCell = getCell();
+//			Cell targetCell = new Cell(getArena(), 15,15);
+			if(Arena.getRandom().nextDouble() < TARGET_PROBABILITY && initialCell != null && targetCell != null && getArena().getNDays() > 360){
+//			if(getDensity(getCell()) > MAX_DENSITY && initialCell != null && targetCell != null){
+				return new MoveToward(getCell(), findDeltaCell(initialCell, targetCell, getCell()), true);
+			}
+		
+		return new Move(Direction.randomDirection());
+	}
+	
 	@Override
-	protected double getInitialGene(GeneType type) {
-		
-		double ranNum = getRandom().nextGaussian() * .1 + .5;
-		
+	protected double getInitialGene(GeneType type) {		
 		switch(type) {
 		case SIZE1:
-			return ranNum;
+			return 0.4610585443862264;
 
 		case SIZE2:
-			return ranNum;
+			return 0.4610585443862264;
 
 		case SPEED1:
-			return ranNum;
+			return 0.47581305626751774;
 
 		case SPEED2:
-			return ranNum;
+			return 0.47581305626751774;
 
 		case MARKINGS1:
-			return ranNum;
+			return 0.48645557972875075;
 
 		case MARKINGS2:
-			return ranNum;
+			return 0.48645557972875075;
 
 		case FERTILITY:
-			return ranNum;
+			return 0.471198253194545;
 
 		default:
 			throw new RuntimeException("Never reach here");
@@ -73,30 +109,66 @@ public class Herbivore3 extends Herbivore {
 	protected double getInitialSD(GeneType type) {
 		switch(type) {
 		case SIZE1:
-			return .1;
+			return 0.5348987192581109;
 
 		case SIZE2:
-			return .1;
+			return 0.5348987192581109;
 
 		case SPEED1:
-			return .1;
+			return 0.4428054025917259;
 
 		case SPEED2:
-			return .1;
+			return 0.4428054025917259;
 
 		case MARKINGS1:
-			return .1;
+			return 0.3260788875192442;
 
 		case MARKINGS2:
-			return .1;
+			return 0.3260788875192442;
 
 		case FERTILITY:
-			return .1;
+			return 0.4936704191700037;
 
 		default:
 			throw new RuntimeException("Never reach here");
 
 		}
 	}
-
+	
+	public double distance(Cell a, Cell b){
+		return Math.sqrt(Math.pow(a.getX() - b.getX(),2) + Math.pow(a.getY() - b.getY(),2));
+	}
+	
+	//Cell where the first animal moved from, Cell where the first animal is moving towards,
+	//Cell that the current animal is moving from
+	public Cell findDeltaCell(Cell initial, Cell target, Cell initialOffset){
+		int deltaX = initial.getX() - initialOffset.getX();
+		int deltaY = initial.getY() - initialOffset.getY();
+		int targetX = target.getX() + deltaX;
+		int targetY = target.getY() + deltaY;
+		if(targetX > getArena().getXDim())
+			targetX = getArena().getXDim();
+		if(targetY > getArena().getYDim())
+			targetY = getArena().getYDim();
+		if(targetX < 0)
+			targetX = 0;
+		if(targetY < 0)
+			targetY = 0;
+		return new Cell(getArena(), targetX, targetY);
+	}
+	
+	public double getDensity(Cell cell){
+		double density = 0;
+		for(int i = 0; i < getArena().getXDim(); i++){
+			for(int j = 0; j < getArena().getYDim(); j++){
+				Cell c = getArena().getCell(i,j);
+				if(distance(getCell(), c) < DENSITY_DISTANCE){
+					density += c.getOtherAnimals(null).size();
+				}
+			}
+		}
+		return density;
+	}
+	
+	
 }
